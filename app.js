f53c3ff74dec155d0955a18a3bdca37f8a1535d8
Basem3sam <basemesam100@gmail.com>
2025-04-24 21:45:34 +0200

    Initial commit with Express setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
