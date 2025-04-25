f96bf54b01922b6099fbc7f4fdd46916f5530ffe
Basem3sam <basemesam100@gmail.com>
2025-04-25 23:39:24 +0300

    fixing Forks

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
+const studentRoutes = require('./routes/studentRoutes');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use('/student', studentRoutes);
 
 const connectDB = require('./config/db');
 connectDB();
